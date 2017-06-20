cf662e7e69fd59da2712b28434f0cb1021782ec8
Your Name <you@example.com>
2017-06-20 06:40:35 -0500

    webbrowser app

--- WebBrowser/ViewController.swift
+++ WebBrowser/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.loadURL(string: "http://jolietboatstore.com")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loadURL(string: String){
+        let requestURL: String!
+        if string.hasPrefix("http://") || string.hasPrefix("https://") {
+            requestURL = string
+        } else {
+            requestURL = "http://\(string).com"
+        }
+        
+        let url = URL(string: requestURL)
+        let request = URLRequest(url: url!)
+        self.webView.loadRequest(request)
     }
-
-
+    
+    @IBAction func navigate(_ sender: UIBarButtonItem) {
+        switch sender.tag{
+        case 10:
+            if self.webView.canGoBack{
+                self.webView.goBack()
+            }
+        case 20:
+            if self.webView.canGoForward{
+                self.webView.goForward()
+            }
+        default:
+            print("Unexpected Sender")
+            
+        }
+    }
 }
 
+extension ViewController: UIWebViewDelegate {
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        self.spinner.startAnimating()
+    }
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        self.spinner.stopAnimating()
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.loadURL(string: textField.text!)
+        return textField.resignFirstResponder()
+    }
+    
+}
